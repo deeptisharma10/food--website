baedbbf3a50d51a3eb23ac775a951724d259898d
deepti sharma <124440255+deeptisharma10@users.noreply.github.com>
2023-08-24 00:00:29 +0530

    added header and hero container

--- js/custom.js
+++ js/custom.js
@@ -19,52 +19,15 @@ $(document).ready(function () {
     autoplay: true,
     infinite: true,
     autoplaySpeed: 5000,
-    arrows: true,
+    arrows: false,
     speed: 1000,
     fade: true,
     adaptiveHeight: true,
     cssEase: "linear",
   });
-  $(".socks-slider").slick({
-    dots: false,
-    infinite: true,
-    autoplay: false,
-    arrows: true,
-    autoplaySpeed: 7000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    accessibility: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
+
+  
+ 
 });
 
 wow = new WOW({
